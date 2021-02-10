b0e6a3095bd74837fc8e3d70bc46131a2cd81ffa
Alvin Leung <alvinleung2009@gmail.com>
2021-02-10 03:39:43 -0800

    Half Implemented section change

--- src/components/navigation/SectionMarker.js
+++ src/components/navigation/SectionMarker.js
@@ -1,10 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { useNavMenu } from "./NavigationContext";
 
 const SECTION_VERTICAL_OFFSET = 200; // vertical offset to compensate the anchor difference
 const SectionMarker = ({ group, label, hash, children }) => {
+  const measureRef = useRef();
   const [navMenu, registerSection] = useNavMenu();
+
+  //TODO: use intersection observer to update the current hashtag
+  useEffect(() => {
+    const options = { rootMargin: `${-SECTION_VERTICAL_OFFSET}px 0px 0px 0px` };
+    const observer = new IntersectionObserver(function (entries, observer) {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        // setSectionInView(true);
+        window.history.pushState({}, "", `#${hash}`);
+      } else {
+        // setSectionInView(false);
+      }
+    }, options);
+
+    // observe the container to see if the section is in view
+    observer.observe(measureRef.current);
+
+    // cleanup
+    return () => observer.unobserve(measureRef.current);
+  }, []);
 
   // register the section to the navigation context
   useEffect(() => {
@@ -16,7 +37,7 @@ const SectionMarker = ({ group, label, hash, children }) => {
   }, []);
 
   return (
-    <div>
+    <div ref={measureRef}>
       <a
         id={hash}
         className="section-anchor"
