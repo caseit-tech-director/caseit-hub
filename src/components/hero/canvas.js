62e0c3be0c3b3ca0baa93788a02579ebd7ae641a
Alvin Leung <alvinleung2009@gmail.com>
2021-02-12 14:28:57 -0800

    Pause Canvas when out of view

--- src/components/hero/canvas.js
+++ src/components/hero/canvas.js
@@ -10,18 +10,50 @@ const Canvas = (props) => {
     let frameCount = 0;
     let animationFrameId;
 
+    let isPlaying = true;
+
     // for canvas initialisation
     if (typeof setup === "function") setup(context, canvas);
 
     const render = () => {
+      if (!isPlaying) return;
+
+      console.log("rendering...");
+
       frameCount++;
       draw(context, frameCount, canvasRef.current);
       animationFrameId = window.requestAnimationFrame(render);
     };
-    render();
+
+    const startAnimation = () => {
+      isPlaying = true;
+      render();
+    };
+    const pauseAnimation = () => {
+      isPlaying = false;
+    };
+
+    // play and pause canvas depends whether it is in view
+    const options = {
+      rootMargin: `0px 0px 0px 0px`,
+    };
+    const observer = new IntersectionObserver(function (entries, observer) {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        // in view
+        startAnimation();
+      } else {
+        // not in view
+        pauseAnimation();
+      }
+    }, options);
+
+    // observe the container to see if the section is in view
+    observer.observe(canvas);
 
     return () => {
       window.cancelAnimationFrame(animationFrameId);
+      observer.unobserve(canvas);
     };
   }, [draw]);
 
