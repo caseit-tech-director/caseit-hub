16e059f2cbc2fbfb5852142b9d3411d1b72b8380
Alvin Leung <alvinleung2009@gmail.com>
2021-02-11 18:36:35 -0800

    Landing Page and top bar

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,18 +1,22 @@
 import React from "react";
+import "./topbar.scss";
 
 const topbar = () => {
-    return(
-        <div className="topbar">
-            <div className="logo-container">
-                <img src="images/CaseIT_Black.png" alt="CaseIT Logo" width="1em"></img>
-            </div>
-            <div className="welcome-txt">
-                <h4>
-                    CaseIT 2021 Welcome Package, Presented by the Beedie School of Business
-                </h4>
-            </div>
+  return (
+    <div className="topbar-container">
+      <div className="topbar center-column main-grid">
+        <div className="logo-container main-grid__nav-bar">
+          <img src="images/CaseIT_Black.png" alt="CaseIT Logo" />
         </div>
-    )
-}
+        {/* <div className="welcome-txt">
+          <h4>
+            CaseIT 2021 Welcome Package, Presented by the Beedie School of
+            Business
+          </h4>
+        </div> */}
+      </div>
+    </div>
+  );
+};
 
 export default topbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
-import React from "react"
-import Chair from "../components/sections/Chair"
-import Navigation from "../components/navigation"
-import SectionMarker from "../components/navigation/SectionMarker"
-import Button from "../components/form/Button"
-import About from "../components/sections/About"
-import Overview from "../components/sections/Overview"
-import Structure from "../components/sections/Structure"
-import Tech from "../components/sections/Tech"
-import OC from "../components/sections/OC"
-import Sponsor from "../components/sections/Sponsor"
-import TopBar from "../components/topbar/topbar"
+import React from "react";
+import Chair from "../components/sections/Chair";
+import Navigation from "../components/navigation";
+import SectionMarker from "../components/navigation/SectionMarker";
+import Button from "../components/form/Button";
+import About from "../components/sections/About";
+import Overview from "../components/sections/Overview";
+import Structure from "../components/sections/Structure";
+import Tech from "../components/sections/Tech";
+import OC from "../components/sections/OC";
+import Sponsor from "../components/sections/Sponsor";
+import TopBar from "../components/topbar/topbar";
 
-import "@styles/main.scss"
-import WelcomePopup from "@components/welcomePopup/"
-import { NavigationContextProvider } from "@components/navigation/NavigationContext"
+import "@styles/main.scss";
+import WelcomePopup from "@components/welcomePopup/";
+import { NavigationContextProvider } from "@components/navigation/NavigationContext";
 
 const Home = () => {
   return (
     <WelcomePopup>
-      <TopBar/>
+      <TopBar />
       <NavigationContextProvider>
         <div className="main-grid center-column">
           <div className="main-grid__nav">
@@ -26,12 +26,16 @@ const Home = () => {
           </div>
           <div className="main-grid__content">
             {/* dummy content for now */}
+            <SectionMarker label="Welcome Package" hash="hello" group="Welcome">
+              <h1 className="display">Welcome Package</h1>
+              <p>for CaseIT 2021, presented by the Beedie School of Business</p>
+            </SectionMarker>
             <SectionMarker
               label="Letter from the Prime Minister"
               hash="letter_prime"
               group="Welcome"
             >
-              <h1 className="display"> Letter from the Prime Minister </h1>
+              <h1> Letter from the Prime Minister </h1>
               <div className="button-ctr">
                 <Button
                   href="https://drive.google.com/file/d/1I_j5-Bf5ED9XoJxkx_0orIlrlJixR7Ky/view?usp=sharing"
@@ -52,7 +56,7 @@ const Home = () => {
               hash="letter_premier"
               group="Welcome"
             >
-              <h1 className="display"> Letter from the Premier </h1>
+              <h1> Letter from the Premier </h1>
               <div className="button-ctr">
                 <Button
                   href="https://drive.google.com/file/d/1PDYFtERFVBEakLZ7vfbMVv6KA1un7V_l/view?usp=sharing"
@@ -62,38 +66,70 @@ const Home = () => {
                 </Button>
               </div>
             </SectionMarker>
-            <SectionMarker label= "Chair's Welcome" hash="chair_welcome" group="Welcome">
+            <SectionMarker
+              label="Chair's Welcome"
+              hash="chair_welcome"
+              group="Welcome"
+            >
               <Chair />
             </SectionMarker>
-            <SectionMarker label= "Dean's Welcome" hash="dean_welcome" group="Welcome">
+            <SectionMarker
+              label="Dean's Welcome"
+              hash="dean_welcome"
+              group="Welcome"
+            >
               <h1 className="display"> Dean's Welcome </h1>
             </SectionMarker>
-            <SectionMarker label="About the Competition" hash="about" group="Information">
-            <About />
+            <SectionMarker
+              label="About the Competition"
+              hash="about"
+              group="Information"
+            >
+              <About />
             </SectionMarker>
-            <SectionMarker label="Week Overview" hash="week_overview" group="Information">
-            <Overview />
+            <SectionMarker
+              label="Week Overview"
+              hash="week_overview"
+              group="Information"
+            >
+              <Overview />
             </SectionMarker>
-            <SectionMarker label="Competition Structure" hash="comp_structure" group="Information">
-            <Structure />
+            <SectionMarker
+              label="Competition Structure"
+              hash="comp_structure"
+              group="Information"
+            >
+              <Structure />
             </SectionMarker>
             <SectionMarker label="Technology" hash="tech" group="Information">
-            <Tech />
+              <Tech />
             </SectionMarker>
-            <SectionMarker label="Meet the Teams" hash="teams" group="Stakeholders">
-            <h1 className="display"> Meet the Teams </h1>
+            <SectionMarker
+              label="Meet the Teams"
+              hash="teams"
+              group="Stakeholders"
+            >
+              <h1 className="display"> Meet the Teams </h1>
             </SectionMarker>
-            <SectionMarker label="Organizing Committee" hash="oc" group="Stakeholders">
-            <OC/>
+            <SectionMarker
+              label="Organizing Committee"
+              hash="oc"
+              group="Stakeholders"
+            >
+              <OC />
             </SectionMarker>
-            <SectionMarker label="Sponsors" hash="sponsors" group="Stakeholders">
-            <Sponsor/>
+            <SectionMarker
+              label="Sponsors"
+              hash="sponsors"
+              group="Stakeholders"
+            >
+              <Sponsor />
             </SectionMarker>
           </div>
         </div>
       </NavigationContextProvider>
     </WelcomePopup>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
