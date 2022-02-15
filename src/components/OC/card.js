a862172010715e55799a5b4983f08b17383947f7
danielxshi <danielshi96@gmail.com>
2022-02-15 07:39:02 -0800

    Adding OC

--- src/components/OC/card.js
+++ src/components/OC/card.js
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
-import "./card.scss";
-import "@styles/main.scss";
-import Images from "./imageData.js";
+import React, { Component } from "react"
+import "./card.scss"
+import "@styles/main.scss"
+import Images from "./imageData.js"
 
 const card = () => {
   return (
     <div className="OC-container">
-      {Images.map((val) => {
+      {Images.map((item, index) => {
         return (
-          <div className="OC-profile">
-            <img loading="lazy" src={val} className="OC-profile__image" />
+          <div className="OC-profile" key={index}>
+            <img loading="lazy" src={item.src} className="OC-profile__image" />
+            <div className="card--info--wrapper">
+              <div className="card--info--content card--info--name"> 
+                <caption>{item.name}</caption>
+              </div>
+              <div className="card--info--content card--info--role">
+                <caption>{item.team}</caption>
+                <caption>{item.position}</caption>
+              </div>
+            </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
-export default card;
+  )
+}
+export default card
